b9726dd274cca43aeff25367a352e6854c7e1b6e
Andrei0921 <rotaruandrei904@gmail.com>
2025-10-02 11:34:54 +0300

    #6 changed tests again

--- backend/src/main/java/cloudflight/integra/backend/controller/UserController.java
+++ backend/src/main/java/cloudflight/integra/backend/controller/UserController.java
@@ -5,12 +5,11 @@ import cloudflight.integra.backend.dto.UserDTO;
 import cloudflight.integra.backend.entity.User;
 import cloudflight.integra.backend.mapper.UserMapper;
 import cloudflight.integra.backend.service.UserService;
-import java.util.Collection;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.Collection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,110 +21,148 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/users")
 @UserApiErrorResponses
 public class UserController {
-  private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-  private final UserService userService;
-  private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-  @Autowired
-  public UserController(UserService userService, PasswordEncoder passwordEncoder) {
-    this.userService = userService;
-    this.passwordEncoder = passwordEncoder;
-  }
-    @Operation(summary = "Get users",
+    @Autowired
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Operation(
+            summary = "Get users",
             operationId = "getUsers",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "OK",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = UserDTO.class))),
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content =
+                                @Content(
+                                        mediaType = "application/json",
+                                        schema = @Schema(implementation = UserDTO.class))),
             })
-  @GetMapping()
-  public ResponseEntity<Collection<UserDTO>> getAllUsers() {
-    logger.info("Received GET request for all users");
+    @GetMapping()
+    public ResponseEntity<Collection<UserDTO>> getAllUsers() {
+        logger.info("Received GET request for all users");
 
-    Collection<User> users = userService.getAllUsers();
-    Collection<UserDTO> userDTOs = UserMapper.toDtoList(users);
-    logger.info("Users retrieved: {}", userDTOs);
-    return ResponseEntity.ok(userDTOs);
-  }
-    @Operation(summary = "Get user by id",
+        Collection<User> users = userService.getAllUsers();
+        Collection<UserDTO> userDTOs = UserMapper.toDtoList(users);
+        logger.info("Users retrieved: {}", userDTOs);
+        return ResponseEntity.ok(userDTOs);
+    }
+
+    @Operation(
+            summary = "Get user by id",
             operationId = "getUserById",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "OK",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = UserDTO.class))),
-                    @ApiResponse(responseCode = "404", description = "Not found")
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content =
+                                @Content(
+                                        mediaType = "application/json",
+                                        schema = @Schema(implementation = UserDTO.class))),
+                @ApiResponse(responseCode = "404", description = "Not found")
             })
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+        logger.info("Received GET request for user with id: {}", id);
 
-  @GetMapping("/{id}")
-  public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
-    logger.info("Received GET request for user with id: {}", id);
-
-    User user = userService.getUser(id);
-    logger.info("User retrieved: {}", user);
-    return ResponseEntity.ok(UserMapper.toDto(user));
-  }
-    @Operation(summary = "Create user", operationId = "createUser",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Created",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = UserDTO.class)))
-            })
-
-  @PostMapping
-  public ResponseEntity<UserDTO> addUser(@RequestBody UserDTO userDto) {
-    logger.info("Received POST request to add user: {}", userDto);
-
-    User userToAdd = UserMapper.fromDto(userDto);
-    User createdUser = userService.addUser(userToAdd);
-    logger.info("User created: {}", createdUser);
-    return ResponseEntity.ok(UserMapper.toDto(createdUser));
-  }
-    @Operation(summary = "Update user", operationId = "updateUser",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Updated",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = UserDTO.class))),
-                    @ApiResponse(responseCode = "404", description = "Not found")
-            })
-  @PutMapping("/{id}")
-  public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDto) {
-    logger.info("Received PUT request to update user with id: {}. Data: {}", id, userDto);
-    if (!id.equals(userDto.getId())) {
-      throw new IllegalArgumentException("ID in path and request body do not match.");
+        User user = userService.getUser(id);
+        logger.info("User retrieved: {}", user);
+        return ResponseEntity.ok(UserMapper.toDto(user));
     }
-    User userToUpdate = UserMapper.fromDto(userDto);
-    userToUpdate.setPassword(passwordEncoder.encode(userToUpdate.getPassword()));
-    User updatedUser = userService.updateUser(userToUpdate);
-    logger.info("User updated: {}", updatedUser);
-    return ResponseEntity.ok(UserMapper.toDto(updatedUser));
-  }
-    @Operation(summary = "Delete user", operationId = "deleteUser",
+
+    @Operation(
+            summary = "Create user",
+            operationId = "createUser",
             responses = {
-                    @ApiResponse(responseCode = "204", description = "deleted",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = UserDTO.class))),
-                    @ApiResponse(responseCode = "404", description = "Not found")
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "Created",
+                        content =
+                                @Content(
+                                        mediaType = "application/json",
+                                        schema = @Schema(implementation = UserDTO.class)))
             })
-  @DeleteMapping("/{id}")
-  public ResponseEntity<UserDTO> delete(@PathVariable Long id) {
-    logger.info("Received DELETE request for user with id: {}", id);
-    userService.deleteUser(id);
-    logger.info("User with id {} deleted.", id);
-    return ResponseEntity.noContent().build();
-  }
-    @Operation(summary = "Get user by email", operationId = "getUserByEmail",
+    @PostMapping
+    public ResponseEntity<UserDTO> addUser(@RequestBody UserDTO userDto) {
+        logger.info("Received POST request to add user: {}", userDto);
+
+        User userToAdd = UserMapper.fromDto(userDto);
+        User createdUser = userService.addUser(userToAdd);
+        logger.info("User created: {}", createdUser);
+        return ResponseEntity.ok(UserMapper.toDto(createdUser));
+    }
+
+    @Operation(
+            summary = "Update user",
+            operationId = "updateUser",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "OK",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = UserDTO.class))),
-                    @ApiResponse(responseCode = "404", description = "Not found")
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "Updated",
+                        content =
+                                @Content(
+                                        mediaType = "application/json",
+                                        schema = @Schema(implementation = UserDTO.class))),
+                @ApiResponse(responseCode = "404", description = "Not found")
             })
-  @GetMapping("/by-email")
-  public ResponseEntity<UserDTO> getUserByEmail(@RequestParam String email) {
-    logger.info("Received GET request for user with email: {}", email);
-    User user = userService.getUserByEmail(email);
-    logger.info("User retrieved by email: {}", user);
-    return ResponseEntity.ok(UserMapper.toDto(user));
-  }
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDto) {
+        logger.info("Received PUT request to update user with id: {}. Data: {}", id, userDto);
+        if (!id.equals(userDto.getId())) {
+            throw new IllegalArgumentException("ID in path and request body do not match.");
+        }
+        User userToUpdate = UserMapper.fromDto(userDto);
+        userToUpdate.setPassword(passwordEncoder.encode(userToUpdate.getPassword()));
+        User updatedUser = userService.updateUser(userToUpdate);
+        logger.info("User updated: {}", updatedUser);
+        return ResponseEntity.ok(UserMapper.toDto(updatedUser));
+    }
+
+    @Operation(
+            summary = "Delete user",
+            operationId = "deleteUser",
+            responses = {
+                @ApiResponse(
+                        responseCode = "204",
+                        description = "deleted",
+                        content =
+                                @Content(
+                                        mediaType = "application/json",
+                                        schema = @Schema(implementation = UserDTO.class))),
+                @ApiResponse(responseCode = "404", description = "Not found")
+            })
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserDTO> delete(@PathVariable Long id) {
+        logger.info("Received DELETE request for user with id: {}", id);
+        userService.deleteUser(id);
+        logger.info("User with id {} deleted.", id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(
+            summary = "Get user by email",
+            operationId = "getUserByEmail",
+            responses = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "OK",
+                        content =
+                                @Content(
+                                        mediaType = "application/json",
+                                        schema = @Schema(implementation = UserDTO.class))),
+                @ApiResponse(responseCode = "404", description = "Not found")
+            })
+    @GetMapping("/by-email")
+    public ResponseEntity<UserDTO> getUserByEmail(@RequestParam String email) {
+        logger.info("Received GET request for user with email: {}", email);
+        User user = userService.getUserByEmail(email);
+        logger.info("User retrieved by email: {}", user);
+        return ResponseEntity.ok(UserMapper.toDto(user));
+    }
 }
--- backend/src/main/java/cloudflight/integra/backend/dto/UserDTO.java
+++ backend/src/main/java/cloudflight/integra/backend/dto/UserDTO.java
@@ -1,7 +1,6 @@
 package cloudflight.integra.backend.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -9,79 +8,78 @@ import java.time.LocalDateTime;
 public class UserDTO {
     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
+
     @Schema(description = "Full name", example = "Andrei")
     private String name;
+
     @Schema(description = "Email address", example = "andrei@gmail.com")
     private String email;
+
     @Schema(description = "Password", example = "password")
     private String password;
+
     @Schema(description = "Account creation timestamp ", example = "2025-09-24T14:35:00")
     private LocalDateTime createdAt;
+
     @Schema(description = "Account balance", example = "1500.75")
     private BigDecimal balance;
 
-  public UserDTO() {}
+    public UserDTO() {}
 
-  public UserDTO(
-      Long id,
-      String name,
-      String email,
-      String password,
-      LocalDateTime createdAt,
-      BigDecimal balance) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.createdAt = createdAt;
-    this.balance = balance;
-  }
+    public UserDTO(Long id, String name, String email, String password, LocalDateTime createdAt, BigDecimal balance) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.balance = balance;
+    }
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public LocalDateTime getCreatedAt() {
-    return createdAt;
-  }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 
-  public BigDecimal getBalance() {
-    return balance;
-  }
+    public BigDecimal getBalance() {
+        return balance;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-  public void setCreatedAt(LocalDateTime createdAt) {
-    this.createdAt = createdAt;
-  }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
-  public void setBalance(BigDecimal balance) {
-    this.balance = balance;
-  }
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
