797cf6b571b035746e427153661c8ded36b59857
EmiCuciu <emicuciu@gmail.com>
2025-10-29 13:33:03 +0200

    ce/#77/landing-page added landing-page completed, we will need to add the /src/styles from user-ui bracnh, for side-bar styling etc.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import {Routes} from '@angular/router';
 import {LoginComponent} from './component/login/login.component';
-import {DashboardComponent} from './component/dashboard/dashboard.component';
-import {AuthGuard} from './guards/auth.guard';
 import {RegisterComponent} from './component/register/register.component';
+import {LandingComponent} from './component/landing/landing.component';
 
 export const routes: Routes = [
+  {path: '', redirectTo: '/landing-page', pathMatch: 'full'},
+  {path: 'landing-page', component: LandingComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
-  {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
