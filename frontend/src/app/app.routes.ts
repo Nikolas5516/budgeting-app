50e485ea6e6c53aad4749bba62ef6e810aab5d02
Nikolas5516 <nikolas.gavaz@stud.ubbcluj.ro>
2025-10-30 13:57:13 +0200

    Frontend progress save

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,3 @@
-import {Routes} from '@angular/router';
-import {LoginComponent} from './component/login/login.component';
-import {AuthGuard} from './guards/auth.guard';
-import {RegisterComponent} from './component/register/register.component';
-import {IncomeListComponent} from './component/incomes/income-list/income-list';
-import {AddIncomeComponent} from './component/incomes/add-income/add-income';
-import {IncomeComponent} from './component/incomes/income_overview/income';
 
 //import { TestComponent } from './component/test-component/test-component';
 import {OverviewComponent} from './component/payments/overview-payments/overview.component';
@@ -13,8 +6,19 @@ import {AddPaymentComponent} from './component/payments/add-payments/add-payment
 import {EditPaymentComponent} from './component/payments/edit-payments/edit-payments.component';
 import {SidebarPaymentComponent} from './component/payments/sidebar/sidebar.component';
 
-import {UserComponent} from './component/user-ui/user.component';
-import {LandingComponent} from './component/landing/landing.component';
+import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { RegisterComponent } from './component/register/register.component';
+import { IncomeListComponent } from './component/incomes/income-list/income-list';
+import { AddIncomeComponent } from './component/incomes/add-income/add-income';
+import { IncomeComponent } from './component/incomes/income_overview/income';
+import { UserComponent } from './component/user-ui/user.component';
+import { LandingComponent } from './component/landing/landing.component';
+import { AllExpensesComponent } from './component/expenses/list-expense/list-expense';
+import { AddExpenseComponent } from './component/expenses/add-expense/add-expense';
+import { EditExpenseComponent } from './component/expenses/edit-expense/edit-expense';
+import { OverviewExpenseComponent } from './component/expenses/overview-expense/overview-expense';
 
 import {SavingsList} from './component/savings/savings-list/savings-list';
 import {SavingsForm} from './component/savings/savings-form/savings-form';
@@ -48,16 +52,6 @@ export const routes: Routes = [
     path: 'sidebar-payments',
     component: SidebarPaymentComponent
   },
-  {path: '', redirectTo: '/landing-page', pathMatch: 'full'},
-  {path: 'landing-page', component: LandingComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  { path: 'income', component: IncomeComponent , canActivate: [AuthGuard]},
-  { path: 'income/list', component: IncomeListComponent , canActivate: [AuthGuard]},
-  { path: 'income/add', component: AddIncomeComponent , canActivate: [AuthGuard]},
-  { path: 'income/edit/:id', component: AddIncomeComponent , canActivate: [AuthGuard]},
-  {path: 'user', component: UserComponent, canActivate: [AuthGuard]},
-
   // Savings routes
   { path: 'savings', component: SavingsOverview },
   { path: 'savings/list', component: SavingsList },
@@ -65,4 +59,18 @@ export const routes: Routes = [
   { path: 'savings/edit/:id', component: SavingsForm },
 
   {path: '', redirectTo: '/login', pathMatch: 'full'}
+  { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
+  { path: 'landing-page', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'income', component: IncomeComponent, canActivate: [AuthGuard] },
+  { path: 'income/list', component: IncomeListComponent, canActivate: [AuthGuard] },
+  { path: 'income/add', component: AddIncomeComponent, canActivate: [AuthGuard] },
+  { path: 'income/edit/:id', component: AddIncomeComponent, canActivate: [AuthGuard] },
+  // Expenses routes
+  { path: 'expenses', component: OverviewExpenseComponent, canActivate: [AuthGuard] },
+  { path: 'expenses/list', component: AllExpensesComponent, canActivate: [AuthGuard] },
+  { path: 'expenses/add', component: AddExpenseComponent, canActivate: [AuthGuard] },
+  { path: 'expenses/edit/:id', component: EditExpenseComponent, canActivate: [AuthGuard] },
+  { path: 'user', component: UserComponent, canActivate: [AuthGuard] },
 ];
